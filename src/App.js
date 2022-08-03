b414349e16179f103758e841615b88651a3cf90a
silentknight2001 <silentknight.www@gmail.com>
2022-08-03 20:09:57 +0530

    Submanu first manu

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Navbar from './Navbar';
+import Hero from './Hero';
+import Submenu from './Submenu'
+import Sidebar from './Sidebar'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Sidebar />
+      <Hero />
+      <Submenu />
+    </>
+  );
+}
+
+export default App;
+
